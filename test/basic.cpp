7692c6073cf9c7969b27d4a9e7e24c5f77a2e59f
mysciz <zhenliangzhou01@gmail.com>
2024-08-29 11:00:18 +0800

    Update Readme Files(Version of Chinese & English)

---
+++ test/basic.cpp
@@ -0,0 +1,7 @@
+#include <iostream> //Defining header files
+using namespace std; //Defining a namespace
+int main()  //main function, program start from here
+{
+    cout << "Hello, world!" << endl; //print Hello World!
+    return 0; //end the program, return 0
+}
